5269ac4f09b2d3fdb50c4a18aa57e1e317b49763
enfini-studios <37464543+enfini-studios@users.noreply.github.com>
2018-03-20 12:54:25 +0530

    swag

--- src/com/enfiniStudios/sigma/ui/CRMController.java
+++ src/com/enfiniStudios/sigma/ui/CRMController.java
@@ -5,7 +5,11 @@ package com.enfiniStudios.sigma.ui;
  * @author "Enfini Studios"
  */
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXProgressBar;
+import com.jfoenix.controls.JFXScrollPane;
+import com.jfoenix.svg.SVGGlyph;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import javafx.fxml.FXML;
@@ -14,39 +18,41 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.scene.input.MouseEvent;
 import static javafx.application.Platform.exit;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
 public class CRMController implements Initializable {
     @FXML
-    private JFXProgressBar loadBar;    
+    private JFXProgressBar loadBar;
+
     
-//   @FXML
-//    private JFXListView<String> contentList;
-//    @FXML
-//    private JFXButton backButton;
-//    @FXML
-//    private JFXScrollPane scroll;
-//    
-//    @FXML
-//    private GridPane navBar;
+    @FXML
+    private JFXListView<String> contentList;
+    @FXML
+    private JFXButton backButton;
+    @FXML
+    private JFXScrollPane scroll;
 
     
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        
+
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         double width = screenSize.getWidth();
         loadBar.setPrefWidth(width);
+
         
         
-//         for (int i = 0; i < 100; i++) {
-//            contentList.getItems().add("Item " + i);
-//        }
-//        contentList.setMaxHeight(3400);
-//
-//        JFXScrollPane.smoothScrolling((ScrollPane) scroll.getChildren().get(0));
+         for (int i = 0; i < 100; i++) {
+            contentList.getItems().add("Item " + i);
+        }
+        contentList.setMaxHeight(3400);
+
+        JFXScrollPane.smoothScrolling((ScrollPane) scroll.getChildren().get(0));
         
-//        navBar.prefWidthProperty().bind(scroll.getBottomBar().widthProperty());
-//        navBar.prefHeightProperty().bind(scroll.getBottomBar().heightProperty());
     }
     
     @FXML
