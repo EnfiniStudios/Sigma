cacdb7bc0f64adc2b3712ea150f2e000b43b84e1
enfini-studios <37464543+enfini-studios@users.noreply.github.com>
2018-03-25 12:01:01 +0530

    Intellij Switch

--- src/com/enfiniStudios/sigma/ui/CRMController.java
+++ src/com/enfiniStudios/sigma/ui/CRMController.java
@@ -6,28 +6,20 @@ package com.enfiniStudios.sigma.ui;
  */
 
 import com.jfoenix.controls.JFXProgressBar;
-import java.awt.Dimension;
-import java.awt.Toolkit;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.scene.input.MouseEvent;
+
 import static javafx.application.Platform.exit;
 
 public class CRMController implements Initializable {
     @FXML
     private JFXProgressBar loadBar;    
     
-//   @FXML
-//    private JFXListView<String> contentList;
-//    @FXML
-//    private JFXButton backButton;
-//    @FXML
-//    private JFXScrollPane scroll;
-//    
-//    @FXML
-//    private GridPane navBar;
 
     
 
@@ -37,16 +29,6 @@ public class CRMController implements Initializable {
         double width = screenSize.getWidth();
         loadBar.setPrefWidth(width);
         
-        
-//         for (int i = 0; i < 100; i++) {
-//            contentList.getItems().add("Item " + i);
-//        }
-//        contentList.setMaxHeight(3400);
-//
-//        JFXScrollPane.smoothScrolling((ScrollPane) scroll.getChildren().get(0));
-        
-//        navBar.prefWidthProperty().bind(scroll.getBottomBar().widthProperty());
-//        navBar.prefHeightProperty().bind(scroll.getBottomBar().heightProperty());
     }
     
     @FXML
